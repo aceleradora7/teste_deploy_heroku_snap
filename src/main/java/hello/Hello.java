c7757fa323db32990a75d9f7f6a667e9f2395624
othman853 <anuar.yasser@gmail.com>
2015-05-29 15:43:25 -0300

    Iniciando projeto

--- src/main/java/hello/Hello.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-/**
- * Created by yasser on 29/05/15.
- */
-public class Hello {
-}
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,9 +1,9 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -13,12 +13,14 @@ public class HelloController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home(){
+    public String index(){
         return "Hello World";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloController.class, args);
+    @RequestMapping(value = "/hi", params = {"name"})
+    @ResponseBody
+    public String hi( @RequestParam(value="name") String value){
+        return "Hi, " + value;
     }
 
 }
