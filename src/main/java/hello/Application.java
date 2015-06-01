60c67cfda56dfb15c4aab7c0d3f1caa6cbeed9b1
Kugra <johan.kovalsikoski@gmail.com>
2015-06-01 11:54:12 -0300

    Printando no console

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Oi");
         SpringApplication.run(Application.class, args);
 
     }
