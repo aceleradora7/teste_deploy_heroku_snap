76282e4bd5c852bf10883f347e29a1aef1efe516
8afael <8afael@gmail.com>
2015-06-01 11:42:34 -0300

    Trocando url hi por oi

--- src/main/java/hello/HiController.java
+++ src/main/java/hello/HiController.java
@@ -3,7 +3,10 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -16,10 +19,14 @@ public class HiController {
 
     }
 
-    @RequestMapping("/hi")
+    @RequestMapping("/oi")
     public String hi( @RequestParam(value="name", required = false, defaultValue = "World") String name, Model model){
         model.addAttribute("name", name);
         return "hi";
     }
 
+
+
 }
+
+
