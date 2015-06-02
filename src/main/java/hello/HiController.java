991a16c088eb28f7233095ff912e2e5d568faed4
othman853 <anuar.yasser@gmail.com>
2015-06-02 01:09:42 -0300

    Reorganizando codigo

--- src/main/java/hello/HiController.java
+++ src/main/java/hello/HiController.java
@@ -3,30 +3,24 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
 public class HiController {
 
+    private static final String HI_VIEW = "hi";
+    private static final String INDEX = "index";
+
     @RequestMapping("/")
-    public String index(){
-
-        return "index";
-
+    public String index() {
+        return INDEX;
     }
 
     @RequestMapping("/hi")
-    public String hi( @RequestParam(value="name", required = false, defaultValue = "World") String name, Model model){
+    public String hi(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hi";
+        return HI_VIEW;
     }
-
-
-
 }
-
 
